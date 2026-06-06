d8f3f8cb20cc57a3233bae66f6ebabf0fc79aff4
yuxihua <3097358949@qq.com>
2026-06-06 16:58:09 +0800

    ３３

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api"
 });
 
 http.interceptors.request.use((config) => {
